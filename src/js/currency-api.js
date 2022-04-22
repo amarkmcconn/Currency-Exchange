82e11fc29ed3cea5fba7050b65ef77ab163d4bc9
Mark McConnell <amark.mcconnell@gmail.com>
2022-04-22 08:42:59 -0700

    setup api call request.

---
+++ src/js/currency-api.js
@@ -0,0 +1,19 @@
+export default class CurrencyService {
+  static getExchange() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/key=${process.env.API_KEY}/latest/USD`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+ 
+ 
