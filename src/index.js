ef1948033624175389f25a17f000615ce5b427e4
Mark McConnell <amark.mcconnell@gmail.com>
2022-04-22 16:13:22 -0700

    updated index.js

--- src/index.js
+++ src/index.js
@@ -20,13 +20,15 @@ $(document).ready(function() {
         if (error.includes("unsupported-code")) {
           $("#show-errors").text(`We currently do not support that currency. Please try a different currency.`);
         } else if (error.includes("malformed-request")) {
-          $("#show-errors").text(`when some part of your request doesn't follow the structure shown above.`);
+          $("#show-errors").text(`When some part of your request doesn't follow the structure shown above.`);
         } else if (error.includes("invalid-key")) {
           $("#show-errors").text(`API key is not valid.`);
         } else if (error.includes("inactive-account")) {
           $("#show-errors").text(`Your email address wasn't confirmed.`);
         } else if (error.includes("quota-reached")) {
           $("#show-errors").text(`Your account has reached the the number of requests allowed by your plan.`);
+        } else{
+          $("#show-errors").text(`No Results found.`);
         }
       });
   });
