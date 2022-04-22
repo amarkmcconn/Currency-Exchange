0bf1ebe8fad7392ed2319498d0b4a58f7bca9ede
Mark McConnell <amark.mcconnell@gmail.com>
2022-04-22 09:40:41 -0700

    WIP UI LOGIC.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyService from './js/currency-api';
+
+$(document).ready(function() {
+  $('#conversion').submit(function (e) {
+    e.preventDefault();
+    let amountUsd = $('#amount').val();
+    let chosenCurrency = $('#conversion').val();
+    let promise = CurrencyService.getExchange();
+      promise.then(function(response) {
+      const body = JSON.parse(response);
+      console.log(body);
+        if (chosenCurrency = "AED") {
+          $('#new-currency').text((`${body.conversion_rates.AED}`) * amountUsd)
+        }
+      })
+  })
+})
